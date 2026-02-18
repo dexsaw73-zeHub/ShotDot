d9639ecb568025f4377393832708d515ea4525cc
dexsaw73-zeHub <dexsaw73@googlemail.com>
2026-02-18 08:26:22 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './shotbot_v1.tsx'],
+  safelist: [
+    // Card scroll full-bleed (ensure these are in production build)
+    'w-[calc(100%+3rem)]',
+    '-ml-6',
+  ],
   theme: {
     extend: {
       fontFamily: {
